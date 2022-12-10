e05d150e75c781cb100daeff57b92b6d6e1ae1bf
devsilva <jerson.silva99@hotmail.com>
2022-12-10 11:06:52 -0500

    feat: Import all icons for the options

--- src/utils/import.js
+++ src/utils/import.js
@@ -2,7 +2,11 @@
 import Preview from '../components/Preview'
 
 // Icons
-import { BsCircle } from 'react-icons/bs'
+import { BsCircle, BsMusicNote } from 'react-icons/bs'
 import { VscChromeClose } from 'react-icons/vsc'
+import { IoMdAttach } from 'react-icons/io'
+import { BiCrop } from 'react-icons/bi'
+import { MdCreate, MdTimer, MdOutlineTextFields } from 'react-icons/md'
+import { FaStickyNote } from 'react-icons/fa'
 
-export { Preview, BsCircle, VscChromeClose }
+export { Preview, BsCircle, VscChromeClose, BsMusicNote, IoMdAttach, BiCrop, FaStickyNote, MdCreate, MdOutlineTextFields, MdTimer }
