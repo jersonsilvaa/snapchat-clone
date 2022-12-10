7d2732bf68cec35672e077e56cf36bb0b5e3ca9c
devsilva <jerson.silva99@hotmail.com>
2022-12-10 09:38:10 -0500

    feat: Add path pewview and error 404, and add containers

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,15 @@ import WebcamCapture from './components/WebcamCapture'
 
 const App = () => {
   return <>
-    <Routes>
-      <Route path='/' element={<WebcamCapture />} />
-    </Routes>
+    <div className="app">
+      <div className="app__body">
+        <Routes>
+          <Route path='/' element={<WebcamCapture />} />
+          <Route path='/preview' element={<h1>Previa de imagen</h1>} />
+          <Route path='*' element={<h1>Error 404</h1>} />
+        </Routes>
+      </div>
+    </div>
   </>
 }
 
