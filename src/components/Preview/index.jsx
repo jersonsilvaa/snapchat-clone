84e5cf61b962e2ed36d537df1b5927951c362ac5
devsilva <jerson.silva99@hotmail.com>
2022-12-10 10:19:03 -0500

    feat: Redirect to the home when the user not capture photo

--- src/components/Preview/index.jsx
+++ src/components/Preview/index.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import { selectCameraImage } from '../../features/cameraSlice'
 
 import './styles.scss'
 
 const Preview = () => {
+    const navigate = useNavigate()
     const cameraImage = useSelector(selectCameraImage)
+
+    useEffect(() => {
+        !cameraImage && navigate('/')
+    }, [cameraImage, navigate])
     return <>
         <div className="preview">
             <h1>Que veo un chad</h1>
