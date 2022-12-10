89b3d9c54edf798818b8b937f3c79c99e714992c
devsilva <jerson.silva99@hotmail.com>
2022-12-10 10:14:19 -0500

    feat: Show the picture

--- src/components/Preview/index.jsx
+++ src/components/Preview/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectCameraImage } from '../../features/cameraSlice'
 
 import './styles.scss'
 
 const Preview = () => {
+    const cameraImage = useSelector(selectCameraImage)
     return <>
-        <div className="preview"></div>
+        <div className="preview">
+            <h1>Que veo un chad</h1>
+            <img src={cameraImage} alt='preview' />
+        </div>
     </>
 }
 
