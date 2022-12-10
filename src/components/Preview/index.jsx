facfecf85c95f99aea40e7d500d49faec3273626
devsilva <jerson.silva99@hotmail.com>
2022-12-10 11:04:25 -0500

    feat: Import more icons and show in the right toolbar

--- src/components/Preview/index.jsx
+++ src/components/Preview/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 
-import { VscChromeClose } from '../../utils/import'
+import { VscChromeClose, MdOutlineTextFields, MdCreate, BsMusicNote, FaStickyNote, IoMdAttach, BiCrop, MdTimer } from '../../utils/import'
 import { resetCameraImage, selectCameraImage } from '../../features/cameraSlice'
 
 import './styles.scss'
@@ -20,8 +20,17 @@ const Preview = () => {
         dispatch(resetCameraImage())
     }
     return <>
-        <div className="preview">
+        <div className='preview'>
             <VscChromeClose className='preview__close' onClick={closePreview} />
+            <div className='preview__toolbarRight'>
+                <MdOutlineTextFields className='svg_icons' />
+                <MdCreate className='svg_icons' />
+                <FaStickyNote className='svg_icons' />
+                <BsMusicNote className='svg_icons' />
+                <IoMdAttach className='svg_icons' />
+                <BiCrop className='svg_icons' />
+                <MdTimer className='svg_icons' />
+            </div>
             <img src={cameraImage} alt='preview' />
         </div>
     </>
