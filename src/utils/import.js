e897e91ae4e0220ae0f7a565edf2d57d62a28565
devsilva <jerson.silva99@hotmail.com>
2022-12-10 10:28:37 -0500

    feat: Import close icon

--- src/utils/import.js
+++ src/utils/import.js
@@ -3,5 +3,6 @@ import Preview from '../components/Preview'
 
 // Icons
 import { BsCircle } from 'react-icons/bs'
+import { GrClose } from 'react-icons/gr'
 
-export { Preview, BsCircle }
+export { Preview, BsCircle, GrClose }
