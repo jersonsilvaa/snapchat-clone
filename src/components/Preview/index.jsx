205277cca0120dc8cdf790d33e900cfa1ecb6b6c
devsilva <jerson.silva99@hotmail.com>
2022-12-10 10:28:18 -0500

    feat: Close the preview image

--- src/components/Preview/index.jsx
+++ src/components/Preview/index.jsx
@@ -1,20 +1,27 @@
 import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import { selectCameraImage } from '../../features/cameraSlice'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { GrClose } from '../../utils/import'
+import { resetCameraImage, selectCameraImage } from '../../features/cameraSlice'
 
 import './styles.scss'
 
 const Preview = () => {
     const navigate = useNavigate()
+    const dispatch = useDispatch()
     const cameraImage = useSelector(selectCameraImage)
 
     useEffect(() => {
         !cameraImage && navigate('/')
     }, [cameraImage, navigate])
+
+    const closePreview = () => {
+        dispatch(resetCameraImage())
+    }
     return <>
         <div className="preview">
-            <h1>Que veo un chad</h1>
+            <GrClose className='preview__close' onClick={closePreview} />
             <img src={cameraImage} alt='preview' />
         </div>
     </>
