528e6e58879bfebf09c4a5ee612fcd09ea751f39
devsilva <jerson.silva99@hotmail.com>
2022-12-10 11:43:39 -0500

    feat: Import send icon

--- src/utils/import.js
+++ src/utils/import.js
@@ -5,8 +5,9 @@ import Preview from '../components/Preview'
 import { BsCircle, BsMusicNote } from 'react-icons/bs'
 import { VscChromeClose } from 'react-icons/vsc'
 import { IoMdAttach } from 'react-icons/io'
+import { IoSend } from 'react-icons/io5'
 import { BiCrop } from 'react-icons/bi'
 import { MdCreate, MdTimer, MdOutlineTextFields } from 'react-icons/md'
 import { FaStickyNote } from 'react-icons/fa'
 
-export { Preview, BsCircle, VscChromeClose, BsMusicNote, IoMdAttach, BiCrop, FaStickyNote, MdCreate, MdOutlineTextFields, MdTimer }
+export { Preview, BsCircle, VscChromeClose, BsMusicNote, IoSend, IoMdAttach, BiCrop, FaStickyNote, MdCreate, MdOutlineTextFields, MdTimer, }
